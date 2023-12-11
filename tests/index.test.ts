f11d645dba6c01e0e056eb80fe1a86b5fadf9d93
Victor Azevedo <victor.azevedo@mail.com>
2023-12-11 07:38:48 +0100

    Use wrangler dev worker for fetching tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,11 +1,27 @@
-import { describe, it, expect, expectTypeOf } from 'vitest'
+import { describe, it, expect, expectTypeOf, beforeAll, afterAll } from 'vitest'
 import { localhost, notfound } from '../src/assets/icons'
 import { dataUriToBlob } from '../src/index'
 import { websites } from '../src/assets/websites'
+import { unstable_dev, UnstableDevWorker } from 'wrangler'
 
-// > wrangler dev
-// > ⎔ Starting local server...
-const origin = 'http://127.0.0.1:8787'
+let worker: UnstableDevWorker
+
+beforeAll(async () => {
+	worker = await unstable_dev('./cloudflare/index.ts', {
+		experimental: { disableExperimentalWarning: true },
+	})
+})
+
+afterAll(async () => {
+	await worker.stop()
+})
+
+describe('Cloudflare worker', () => {
+	it('is not returning server error status code', async () => {
+		const resp = await worker.fetch('/')
+		expect(resp.status < 500).toBe(true)
+	})
+})
 
 describe('Static icons', function () {
 	it('are both SVG data URIs', function () {
@@ -36,99 +52,100 @@ describe('Website list', function () {
 
 describe('Fetching', function () {
 	it('returns notfound icon on empty query', async function () {
-		const response = await fetch(origin + '')
+		const response = await worker.fetch('')
 		expect(response.status).toBe(200)
 		expect(await response.text()).toBe(notfound)
 	})
 
 	it('returns notfound icon on bad query', async function () {
-		const response = await fetch(origin + '/text/drgrdrdhwrdehrwjherwjh')
+		const response = await worker.fetch('/text/drgrdrdhwrdehrwjherwjh')
 		expect(response.status).toBe(200)
 		expect(await response.text()).toBe(notfound)
 	})
 
 	it('returns notfound icon when no protocols are specified', async function () {
-		const response = await fetch(origin + '/text/victr.me/')
+		const response = await worker.fetch('/text/victr.me/')
 		expect(response.status).toBe(200)
 		expect(await response.text()).toBe(notfound)
 	})
 
 	it('gets favicon as text when no type specified', async function () {
-		const response = await fetch(origin + '/https://victr.me')
+		const response = await worker.fetch('/https://victr.me')
 		expect(response.status).toBe(200)
 		expect(await response.text()).toContain('https://')
 	})
 
 	it('gets favicon as text with old /get/ path', async function () {
-		const response = await fetch(origin + '/get/https://victr.me')
+		const response = await worker.fetch('/get/https://victr.me')
 		expect(response.status).toBe(200)
 		expect(await response.text()).toContain('https://')
 	})
 
 	it('gets favicon as text', async function () {
-		const response = await fetch(origin + '/text/https://victr.me')
+		const response = await worker.fetch('/text/https://victr.me')
 		expect(response.status).toBe(200)
 		expect(await response.text()).toBe('https://victr.me/apple-touch-icon.png')
 	})
 
 	it('gets favicon as blob', async function () {
-		const response = await fetch(origin + '/blob/https://victr.me')
+		const response = await worker.fetch('/blob/https://victr.me')
 		expect(response.status).toBe(200)
 		expect((await response.blob())?.type).toBe('image/png')
 	})
 
 	it('returns localhost icon with http://127.0.0.1/', async function () {
-		const response = await fetch(origin + '/text/http://127.0.0.1/')
+		const response = await worker.fetch('/text/http://127.0.0.1/')
 		expect(response.status).toBe(200)
 		expect(await response.text()).toBe(localhost)
 	})
 
 	it('returns localhost icon with http://localhost/', async function () {
-		const response = await fetch(origin + '/text/http://localhost/')
+		const response = await worker.fetch('/text/http://localhost/')
 		expect(response.status).toBe(200)
 		expect(await response.text()).toBe(localhost)
 	})
 
 	describe('examples', function () {
-		let response: Response
+		let response: Awaited<ReturnType<typeof worker.fetch>>
 
 		it('ikea.com', async function () {
-			response = await fetch(origin + '/text/https://ikea.com')
+			response = await worker.fetch('/text/https://ikea.com')
 			expect((await response.text()) !== notfound).toBe(true)
 		})
 
 		it('vitest.dev', async function () {
-			response = await fetch(origin + '/text/https://vitest.dev')
+			response = await worker.fetch('/text/https://vitest.dev')
 			expect((await response.text()) !== notfound).toBe(true)
 		})
 
 		it('steamcharts.com', async function () {
-			response = await fetch(origin + '/text/https://steamcharts.com')
-			expect((await response.text()) !== notfound).toBe(true)
-		})
-
-		it.todo('fr.aliexpress.com is timing out')
-		// it('fr.aliexpress.com', async function () {
-		// 	response = await fetch(origin + '/text/https://fr.aliexpress.com')
-		// 	expect((await response.text()) !== notfound).toBe(true)
-		// })
-
-		it('microsoftedge.microsoft.com', async function () {
-			const url = 'https://microsoftedge.microsoft.com/addons/detail/ublock-origin/odfafepnkmbhccpbejgmiehpchacaeak'
-			response = await fetch(origin + '/text/' + url)
+			response = await worker.fetch('/text/https://steamcharts.com')
 			expect((await response.text()) !== notfound).toBe(true)
 		})
 
 		it('guide.michelin.com', async function () {
-			const url = 'https://guide.michelin.com/fr/fr/restaurants'
-			response = await fetch(origin + '/text/' + url)
+			response = await worker.fetch('/text/https://guide.michelin.com/fr/fr/restaurants')
 			expect((await response.text()) !== notfound).toBe(true)
 		})
 
 		it('help.fr.shopping.rakuten.net', async function () {
-			const url = 'https://help.fr.shopping.rakuten.net'
-			response = await fetch(origin + '/text/' + url)
+			response = await worker.fetch('/text/https://help.fr.shopping.rakuten.net')
 			expect((await response.text()) !== notfound).toBe(true)
 		})
+
+		it('microsoftedge.microsoft.com', async function () {
+			const url = 'https://microsoftedge.microsoft.com/addons/detail/ublock-origin/odfafepnkmbhccpbejgmiehpchacaeak'
+			response = await worker.fetch('/text/' + url)
+			expect((await response.text()) !== notfound).toBe(true)
+		})
+
+		it(
+			'fr.aliexpress.com',
+			async function () {
+				response = await worker.fetch('/text/https://fr.aliexpress.com')
+				expect((await response.text()) !== notfound).toBe(true)
+			},
+			{ timeout: 10000 }
+		)
 	})
 })
