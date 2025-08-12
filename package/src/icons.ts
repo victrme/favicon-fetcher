8e5d9a4d4ad36fdbee816a1fe2aca7d0b144fabc
Tahoe <50768582+morceaudebois@users.noreply.github.com>
2025-08-12 14:15:22 +0200

    Added Ambank & Google Spreadsheets

--- package/src/icons.ts
+++ package/src/icons.ts
@@ -35,6 +35,7 @@ export default {
 		"gemini.google.svg": ["gemini.google.com"],
 		"drive.google.png": ["drive.google.com"],
 		"icloud.mail.png": ["icloud.com/mail/"],
+		"sheets.google.svg": ["docs.google.com/spreadsheets"],
 		"amazon.svg": [
 			"amazon.com",
 			"amazon.ca",
@@ -43,6 +44,7 @@ export default {
 			"amazon.it",
 			"amazon.co.uk",
 		],
+		"ambank.png": ["amonline.com"],
 
 		/* Captcha protected websites */
 		"shopify.png": ["shopify.com"],
